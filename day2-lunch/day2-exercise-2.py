77e011534146185f67b0f9f474a31386fc151a58
Emma Dallon <dallone314@gmail.com>
2018-08-28 12:55:45 -0400

    Perfectly matched alignments

---
+++ day2-lunch/day2-exercise-2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import sys
+
+if len(sys.argv) > 1:
+    inFile = open(sys.argv[1])
+else :
+    #This stdin allows the python script to be piped to and from in the commandline
+    inFile = sys.stdin
+  
+nomismatch_count = 0 
+for i, line in enumerate( inFile ) :
+    if line.startswith("@"):
+        continue
+    fields = line.rstrip("\r\n").split("\t")
+    if fields[1] != "4":
+            if fields[14] == "XM:i:0":
+                nomismatch_count += 1
+   
+print (nomismatch_count)
