dd798139db8c337b67fd50b87d18683582ff2136
Emma Dallon <dallone314@gmail.com>
2018-08-31 16:36:48 -0400

    Updated to remove negative values

--- day5-lunch/day5-exercise2.py
+++ day5-lunch/day5-exercise2.py
@@ -9,7 +9,7 @@ import sys
 inFile = open(sys.argv[1])
 outFile = open(sys.argv[2], 'w')
 
-outFile.write("chromosome" + "\t" + "start" + "\t" + "end" + "\t" + "t_name" + "\n")
+#outFile.write("chromosome" + "\t" + "start" + "\t" + "end" + "\t" + "t_name" + "\n")
 
 for i, line in enumerate(inFile):
     if i == 0:
@@ -20,9 +20,17 @@ for i, line in enumerate(inFile):
     gene_end = int(fields[4])
     t_name = fields[5]
     if fields[2] == "+":
-        p_start = gene_start - 500
-        p_end = gene_start + 500
+        if gene_start in range(500):
+            p_start = gene_start
+            p_end = gene_start + 500
+        else:
+            p_start = gene_start - 500
+            p_end = gene_start + 500
     elif fields[2] == "-":
-        p_start = gene_end + 500
-        p_end = gene_end - 500
+        if gene_end in range(500):
+            p_start = gene_end + 500
+            p_end = gene_end
+        else:
+            p_start = gene_end + 500
+            p_end = gene_end -500
     outFile.write(chr_name + "\t" + str(p_start) + "\t" + str(p_end) + "\t" + t_name + "\n")
