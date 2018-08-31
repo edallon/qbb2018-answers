b7598b2eada982fe18f47b4c486bf146105a1324
Emma Dallon <dallone314@gmail.com>
2018-08-30 20:17:04 -0400

    MA plots

---
+++ day4-homework/ma_plot.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+"""
+Usage: ./day4-exercise2.py <ctab_file1> <ctab_file2>
+"""
+
+import sys
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv(sys.argv[1], sep="\t", index_col="t_name")
+
+name1 = sys.argv[1].split(os.sep)[-2]
+fpkm1 = pd.read_csv(sys.argv[1], sep="\t", index_col="t_name").loc[:, "FPKM"]
+
+name2 = sys.argv[2].split(os.sep)[-2]
+fpkm2 = pd.read_csv(sys.argv[2], sep="\t", index_col="t_name").loc[:, "FPKM"]
+
+x = (1/2)*(fpkm1+fpkm2)
+log_x = np.log(x + 1)
+y = fpkm1/fpkm2
+log_y = np.log(y + 1)
+
+
+# fit1 = np.polyfit(log_fpkm1, log_fpkm2, 1)
+# fit2 = np.poly1d(fit1)
+#
+# x_new = np.linspace (min(log_fpkm1), max(log_fpkm1), 100)
+# y_new = fit2(x_new)
+
+fig, ax = plt.subplots()
+ax.set_title("MA plot")
+ax.set_xlabel("average of {0} and {1}".format(name1, name2))
+ax.set_ylabel("{0}/{1}".format(name1, name2))
+ax.scatter(log_x, log_y, alpha=0.5)
+#ax.plot(x, y, 'k-')
+fig.savefig("MA.png")
+plt.close(fig)
