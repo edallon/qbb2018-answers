85a1685e2698fde38f615dbb61dc160499fc3ac4
Emma Dallon <dallone314@gmail.com>
2018-08-31 12:32:24 -0400

    Approx promoter start and end

---
+++ day5-lunch/day5-exercise2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+"""
+Usage: ./day5-exercise2.py <ctab_file input> <.bed file output>
+"""
+
+import sys
+
+inFile = open(sys.argv[1])
+outFile = open(sys.argv[2], 'w')
+
+outFile.write("chromosome" + "\t" + "start" + "\t" + "end" + "\t" + "t_name" + "\n")
+
+for i, line in enumerate(inFile):
+    if i == 0:
+        continue
+    fields = line.rstrip("\r\n").split("\t")
+    chr_name = fields[1]
+    gene_start = int(fields[3])
+    gene_end = int(fields[4])
+    t_name = fields[5]
+    if fields[2] == "+":
+        p_start = gene_start - 500
+        p_end = gene_start + 500
+    elif fields[2] == "-":
+        p_start = gene_end + 500
+        p_end = gene_end - 500
+    outFile.write(chr_name + "\t" + str(p_start) + "\t" + str(p_end) + "\t" + t_name + "\n")
