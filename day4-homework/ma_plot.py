0c669ce5599648686c24a4b8be1a2342b28d72b8
Emma Dallon <dallone314@gmail.com>
2018-08-31 09:27:53 -0400

    log2 MA plot

--- day4-homework/ma_plot.py
+++ day4-homework/ma_plot.py
@@ -18,10 +18,10 @@ fpkm1 = pd.read_csv(sys.argv[1], sep="\t", index_col="t_name").loc[:, "FPKM"]
 name2 = sys.argv[2].split(os.sep)[-2]
 fpkm2 = pd.read_csv(sys.argv[2], sep="\t", index_col="t_name").loc[:, "FPKM"]
 
-x = (1/2)*(fpkm1+fpkm2)
-log_x = np.log(x + 1)
-y = fpkm1/fpkm2
-log_y = np.log(y + 1)
+x = (1/2)*np.log2((fpkm1+1)+(fpkm2+1))
+#log_x = np.log(x + 1)
+y = np.log2((fpkm1+1)/(fpkm2+1))
+#log_y = np.log(y + 1)
 
 
 # fit1 = np.polyfit(log_fpkm1, log_fpkm2, 1)
@@ -34,7 +34,7 @@ fig, ax = plt.subplots()
 ax.set_title("MA plot")
 ax.set_xlabel("average of {0} and {1}".format(name1, name2))
 ax.set_ylabel("{0}/{1}".format(name1, name2))
-ax.scatter(log_x, log_y, alpha=0.5)
+ax.scatter(x, y, alpha=0.5)
 #ax.plot(x, y, 'k-')
-fig.savefig("MA.png")
+fig.savefig("MA-1.png")
 plt.close(fig)
